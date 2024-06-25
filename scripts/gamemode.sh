018b414e8f901e9c766e6a50cf0b21aa417bb161
GabriWar <gabrielduarteg02@gmail.com>
2024-06-25 15:28:48 -0300

    fixed gamemode

--- scripts/gamemode.sh
+++ scripts/gamemode.sh
@@ -9,6 +9,12 @@ if [ "$HYPRGAMEMODE" = 1 ]; then
         keyword general:gaps_out 0;\
         keyword general:border_size 1;\
         keyword decoration:rounding 0"
+	killall -q waybar
+	killall -q swww
+	killall -q swww-daemon
 	exit
 fi
-hyprctl reload
+hyprctl reload &
+swww-daemon &
+sh ~/scripts/waybar.sh &
+exit
