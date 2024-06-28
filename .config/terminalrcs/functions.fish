45d34be27d5614618c3af480f27414b90f7bafff
GabriWar <gabrielduarteg02@gmail.com>
2024-06-28 16:43:29 -0300

    godly keybinds again

--- .config/terminalrcs/functions.fish
+++ .config/terminalrcs/functions.fish
@@ -8,23 +8,25 @@ function fzffiles
     set selected_files (fd --type f -d 6 -H | fzf --multi --prompt="Select files: " --preview "bat --color=always {}")
     if test -n "$selected_files"
         set -l files (echo $selected_files | tr '\n' ' ')
-        eval $cmd $files
+        commandline -r "$cmd $files"
     else
         echo "No files selected."
     end
 end
 
 function fzfgrep
+    set cmd (commandline)
     set file (fd -t f -d 6 -H | xargs grep -n . | fzf --ansi | awk -F: '{print $1}')
     if test -n "$file"
-        commandline -t "$file"
+        commandline -r "$cmd $file"
     end
 end
 
 function fzfall
+    set cmd (commandline)
     set selected_file (fzf --prompt="Select file: " --preview "bat --color=always {}" --preview-window=right:60%)
     if test -n "$selected_file"
-        commandline -t "$selected_file"
+        commandline -r "$cmd $selected_file"
     else
         echo "No file selected."
     end
