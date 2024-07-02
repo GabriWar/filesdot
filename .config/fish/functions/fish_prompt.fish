f346d0c4b461266064c82de5dd5aef6c9b3f776a
Gabriel Duarte Guerra <gabrielduarteg02@gmail.com>
2024-07-02 15:40:01 -0300

    goated prompr shows battery now

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -114,7 +114,7 @@ function fish_prompt
 
     # Battery status
     type -q acpi
-    and test (acpi -a 2> /dev/null | string match -r off)
+    and test (acpi -a 2> /dev/null)
     and _nim_prompt_wrapper $retc B (acpi -b | cut -d' ' -f 4-)
 
     # New line
