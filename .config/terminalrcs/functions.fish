80021fb88cf334be083cd6d9cd7bdcbe11ecb72b
Gabriel Duarte Guerra <gabrielduarteg02@gmail.com>
2024-07-05 19:25:47 -0300

    moar depth

--- .config/terminalrcs/functions.fish
+++ .config/terminalrcs/functions.fish
@@ -5,7 +5,7 @@ end
 
 function fzffiles
     set cmd (commandline)
-    set selected_files (fd --type f -d 6 -H | fzf --multi --prompt="Select files: " --preview "bat --color=always {}")
+    set selected_files (fd --type f -d 10 -H | fzf --multi --prompt="Select files: " --preview "bat --color=always {}")
     if test -n "$selected_files"
         set -l files (echo $selected_files | tr '\n' ' ')
         commandline -r "$cmd $files"
@@ -16,7 +16,7 @@ end
 
 function fzfgrep
     set cmd (commandline)
-    set file (fd -t f -d 6 -H | xargs grep -n -I . | fzf --ansi | awk -F: '{print $1}')
+    set file (fd -t f -d 10 -H | xargs grep -n -I . | fzf --ansi | awk -F: '{print $1}')
     if test -n "$file"
         commandline -r "$cmd $file"
     end
