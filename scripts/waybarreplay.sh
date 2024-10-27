c461602c4ba56f4e8833cbc85c61c96bb9d16832
Gabriel Duarte Guerra <gabrielduarteg02@gmail.com>
2024-10-27 15:35:06 -0300

    FULLY CODDED "NVIDIA REPLAY FEATURE"

---
+++ scripts/waybarreplay.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+on="󰑙"
+off="󰑚"
+
+# Check if the argument 'toggle' is passed
+if [ "$1" == "toggle" ]; then
+	if pgrep -x "gpu-screen-reco" >/dev/null; then
+		killall -SIGINT gpu-screen-recorder
+		notify-send "Stopping replay"
+
+	else
+		gpu-screen-recorder -r 120 -f 60 -w screen -o $HOME/Videos/ -c mp4 -q ultra -a "default_output|default_input" >/dev/null &
+		notify-send "Starting replay"
+
+	fi
+fi
+
+# Check if the argument 'save' is passed
+if [ "$1" == "save" ]; then
+	if pgrep -x "gpu-screen-reco" >/dev/null; then
+		killall -SIGUSR1 gpu-screen-recorder
+		notify-send "Saving replay"
+	fi
+fi
+
+if pgrep -x "gpu-screen-reco" >/dev/null; then
+	ONOFF=$on
+	ONOFFSTRING="ON"
+else
+	ONOFF=$off
+	ONOFFSTRING="OFF"
+fi
+
+echo "{ \"text\": \"$ONOFF\", \"tooltip\":\"IS $ONOFFSTRING\n Press ctrl a to save replay \n press ctrl b to stop \"}"
