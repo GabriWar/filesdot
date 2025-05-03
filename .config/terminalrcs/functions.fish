01632af8f68c72db5040956332a3069c1c5dd1d2
GabriWar <gabrielduarteg02@gmail.com>
2025-05-02 21:39:40 -0300

    better fzf

--- .config/terminalrcs/functions.fish
+++ .config/terminalrcs/functions.fish
@@ -1,30 +1,71 @@
+# FZF default options
 set --export FZF_DEFAULT_OPTS '--cycle --layout=reverse --border --height=90% --preview-window=wrap --marker="*"'
+
+# Bring background job to foreground via fzf
 function fzfjobs
-    set selected_pid (jobs | fzf | awk '{print $2}')
+    set selected_pid (jobs | fzf --prompt="Select job: " | awk '{print $2}')
     and fg %$selected_pid
 end
 
+# Insert selected files at command line
 function fzffiles
     set cmd (commandline)
-    set selected_files (fd --type f -d 100 -H | fzf --multi --prompt="Select files: " --preview "bat --color=always {}")
+    set selected_files (fd --type f -d 100 -H | fzf --multi --prompt="Select files: " --preview "bat --style=numbers --color=always {}" 2>/dev/null)
     if test -n "$selected_files"
-        set -l files (echo $selected_files | tr '\n' ' ')
-        commandline -r "$cmd $files"
+        commandline -r "$cmd $selected_files"
     end
 end
+
+# Grep and insert matched file
 
 function fzfgrep
     set cmd (commandline)
-    set file (fd -t f -d 10 -H | xargs grep -n -I . | fzf --ansi --preview "" | awk -F: '{print $1}')
-    if test -n "$file"
+    set match (
+        rg --line-number --no-heading --color=always . 2>/dev/null \
+        | fzf --ansi --prompt="Grep file: " \
+              --delimiter=: \
+              --preview='bat --style=numbers --color=always {1} --highlight-line {2}' \
+              --preview-window=up:70%
+    )
+
+    if test -n "$match"
+        set file (echo $match | cut -d: -f1)
         commandline -r "$cmd $file"
     end
 end
-
+# File selector with preview
 function fzfall
     set cmd (commandline)
-    set selected_file (fzf --prompt="Select file: " --preview "bat --color=always {}" --preview-window=right:60%)
+    set selected_file (fzf --prompt="Select file: " --preview "bat --style=numbers --color=always {}" --preview-window=right:60%)
     if test -n "$selected_file"
         commandline -r "$cmd $selected_file"
+    end
+end
+
+# Show which package owns a command
+function whoown
+    set cmd (command -v $argv[1] 2>/dev/null)
+    if test -n "$cmd"
+        pacman -Qo $cmd
+    else
+        echo "Command not found: $argv[1]" >&2
+    end
+end
+
+function fzfnvim
+    set match (
+        rg --line-number --no-heading --color=always . 2>/dev/null \
+        | fzf --ansi --prompt="Grep & open in Neovim: " \
+              --delimiter=: \
+              --preview='bat --style=numbers --color=always {1} --highlight-line {2}' \
+              --preview-window=up:70%
+    )
+
+    if test -n "$match"
+        set file (echo $match | cut -d: -f1)
+        set line (echo $match | cut -d: -f2)
+        if test -n "$file" -a -n "$line"
+            nvim +"$line" "$file"
+        end
     end
 end
