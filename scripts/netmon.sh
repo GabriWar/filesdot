fbdba0d8e1a42281f6dd2306ca66600ceaa4a55e
GabriWar <gabrielduarteg02@gmail.com>
2025-05-12 23:17:04 -0300

    remappppsss

--- scripts/netmon.sh
+++ scripts/netmon.sh
@@ -2,4 +2,4 @@
 #find this script path
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 #exec python3
-python3 "$DIR/netmon/netmon.py" "-wNi 3"
+python3 "$DIR/netmon/netmon.py" "-wNi 3 --only-new"
